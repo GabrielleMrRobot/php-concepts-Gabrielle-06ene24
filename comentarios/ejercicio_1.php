a99544de0b3e06b5849bdf332adea438381c9682
Leosmer <Leosmervillegas@gmail.com>
2024-01-06 17:55:23 -0400

    feat: finish php-concepts-GABRIELLE-06ene24.git

---
+++ comentarios/ejercicio_1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  //Este es un comentario de una linea
+
+  # Este tambien es un comentario de una linea
+
+  /* Este es un comentario multi-linea */
+
+  echo "Bienvenido a casa"; // Salida de un mensaje de bienvenida
+  ?>
+
+  <?= "Bienvenido a casa"; ?>
+  
+  <?php //echo "bienvenido a casa"; ?>
+</body>
+</html>
---
+++ sintaxis/ejercicio_3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Mi primera pagina php</h1>
+  <?php
+    $color = "red";
+
+    echo "Mi carro es " . $color . "<b>";
+    echo "Mi casa es " . $COLOR . "<b>";
+    echo "Mi carro es " . $color . "<b>";
+    ?>
+</body>
+</html>
